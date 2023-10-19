f526e70b889737215f09a627114d4520e1375f50
runny <watcharit_s@cmu.ac.th>
2023-10-20 03:01:00 +0700

    committee

--- src/views/Advisor/AdvisorStudentView.vue
+++ src/views/Advisor/AdvisorStudentView.vue
@@ -9,21 +9,26 @@ defineProps({
             require: true
     }
 })
+
 </script>
 
 
 <template>
+
   <div class="flex justify-center items-start min-h-screen animate__animated animate__zoomIn">
     <div v-for="advisees in advisor?.ownStudents" :key="advisees.id" class="card bg-base-100 shadow-xl lg:w-96 p-6 m-5">
+      <RouterLink  :to="{name: 'studentdetail', params:{id: advisees.id}}">
+
       <div class="card-body">
         <img v-for="image in advisees.images" :key="image" :src="image" alt="student image"/>
-        <h2 class="card-title text-xl font-semibold mb-2">{{ advisees.name }} {{ advisees.surname }}</h2>
         
+        <h2 class="card-title text-xl font-semibold mb-2">{{ advisees.name }} {{ advisees.surname }}</h2>
         <p>Student ID: {{ advisees.studentid }}</p>
         <p>Department: {{ advisees.department }}</p>
         <p>Location: {{ advisees.location }}</p>
-
       </div>
+    </RouterLink>
+
     </div>
   </div>
 </template>
--- src/views/StudentFormView.vue
+++ src/views/StudentFormView.vue
@@ -1,57 +1,97 @@
+<script setup lang="ts">
+import type { StudentInfo } from '@/student'
+import { ref } from 'vue'
+import StudentService from '../service/StudentService'
+import { useRouter } from 'vue-router'
+import { useMessageStore } from '@/stores/message'
+import BaseInput from '../components/BaseInput.vue'
+import ImageUpload from '../components/ImageUpload.vue'
+const store = useMessageStore()
+
+
+const router = useRouter()
+function saveStudent() {
+  StudentService.saveStudent(student.value)
+        .then((response) => {
+            console.log(response.data)
+            router.push({
+                name: 'student',
+                params: { id: response.data.id }
+            })
+                        store.updateMessage('You are successfully add a new Student!!')
+            setTimeout(() => {
+                store.resetMessage()
+            }, 3000)
+
+        }).catch(() => {
+            router.push({ name: 'network-error' })
+        })
+}
+
+const student = ref<StudentInfo>({
+    id: 0,
+    studentid: '',
+    name: '',
+    surname: '',
+    department: '',
+    location: '',
+    images: [],
+    advisor: {id: 0,name:'',surname:'', academic_position:'',department: '', images:''},
+    courses: []
+})
+
+</script>
+
 <template>
 <div class="max-w-md mx-auto mt-8 p-6 bg-white shadow-lg rounded-lg animate__animated animate__fadeIn">
   <h2 class="text-2xl font-semibold mb-4">Add Student</h2>
-  <form>
+  <form @submit.prevent="saveStudent">
     <div class="mb-4">
       <label for="studentId" class="block text-sm font-medium text-gray-600">Student ID:</label>
-      <input
-        type="text"
-        id="studentId"
-        name="studentId"
-        class="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-200"
-        placeholder="Enter Student ID"
-      />
+      <BaseInput
+          v-model="student.studentid"
+           type="text"
+           class="border border-gray-300 rounded-md py-2 px-4 w-full focus:outline-none focus:ring focus:border-blue-300 mt-1"
+           />
     </div>
     <div class="mb-4">
       <label for="name" class="block text-sm font-medium text-gray-600">Name:</label>
-      <input
-        type="text"
-        id="name"
-        name="name"
-        class="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-200"
-        placeholder="Enter Name"
-      />
+      <BaseInput
+          v-model="student.name"
+           type="text"
+           class="border border-gray-300 rounded-md py-2 px-4 w-full focus:outline-none focus:ring focus:border-blue-300 mt-1"
+
+           />
     </div>
     <div class="mb-4">
       <label for="surname" class="block text-sm font-medium text-gray-600">Surname:</label>
-      <input
-        type="text"
-        id="surname"
-        name="surname"
-        class="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-200"
-        placeholder="Enter Surname"
-      />
+      <BaseInput
+          v-model="student.surname"
+           type="text"
+           class="border border-gray-300 rounded-md py-2 px-4 w-full focus:outline-none focus:ring focus:border-blue-300 mt-1"
+
+           />
     </div>
     <div class="mb-4">
       <label for="department" class="block text-sm font-medium text-gray-600">Department:</label>
-      <input
-        type="text"
-        id="department"
-        name="department"
-        class="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-200"
-        placeholder="Enter Department"
-      />
+      <BaseInput
+          v-model="student.department"
+           type="text"
+           class="border border-gray-300 rounded-md py-2 px-4 w-full focus:outline-none focus:ring focus:border-blue-300 mt-1"
+
+           />
     </div>
     <div class="mb-4">
       <label for="location" class="block text-sm font-medium text-gray-600">Location:</label>
-      <input
-        type="text"
-        id="location"
-        name="location"
-        class="mt-1 p-2 w-full border rounded-md focus:ring focus:ring-blue-200"
-        placeholder="Enter Location"
-      />
+      <BaseInput
+          v-model="student.location"
+           type="text"
+           class="border border-gray-300 rounded-md py-2 px-4 w-full focus:outline-none focus:ring focus:border-blue-300 mt-1"
+
+           />
     </div>
+    <h3>Add the image here</h3>
+    <image-upload v-model="student.images" />
     <button
       type="submit"
       class="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200"
@@ -59,6 +99,8 @@
       Add Student
     </button>
   </form>
+
 </div>
 
 </template>
+
