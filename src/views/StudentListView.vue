02eb400879e93febceff01d31a96d1f1fa564e5c
runny <watcharit_s@cmu.ac.th>
2023-10-21 23:59:31 +0700

    clean no v-if

--- src/views/StudentListView.vue
+++ src/views/StudentListView.vue
@@ -85,7 +85,7 @@ const hasNextPage = computed(()=>{
     </div>
     <div class="flex justify-end items-center flex-wrap mr-6">
       <router-link :to="{name:'studentform'}">
-      <button v-if="authStore.currentUserName" type="button" class="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-auto">
+      <button type="button" class="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-auto">
         + Add Student
       </button>
     </router-link>
